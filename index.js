8f557c038225f9dcd48ea5aa213b97080496c3a5
Mohammed  Abdulaziz <mohammedazizbuss@gmail.com>
2019-10-27 14:51:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let Card=[];
 function writeCards(Arr,event){
     
@@ -14,6 +15,21 @@ writeCards(['Ali', 'Mohammed', 'Amro'],'birthday');
  console.log(Card);
 function countdown(Num){
     let i=Num;
+=======
+function writeCards(arr,event){
+    Card=[];
+    for(let i=0;i<arr.length;i++){
+        Card[i]="Thank you,"+arr[i]+" for the wonderful "+event+" gift!";
+       
+    }
+        console.log(Card);
+}
+writeCards(["Mohammed", "Amro", "Adeeb"],"birthday");
+
+/* ///////////////////////////////////////////While//////////////////////////////////////*/
+function countdown(Num){
+    i=Num;
+>>>>>>> c065ff9c00fbe8833dbe2445385e70a5a64c01c5
     while(i>=0){
         console.log(i);
         i--;
