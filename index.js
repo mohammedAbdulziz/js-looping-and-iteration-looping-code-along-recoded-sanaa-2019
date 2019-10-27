dbb4378550af2f7fd812e73ca41047710ccb5bef
Mohammed  Abdulaziz <mohammedazizbuss@gmail.com>
2019-10-27 09:00:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+let Card=[];
+function writeCards(Arr,event){
+    
+    for(let i=0;i<Arr.length;i++){
+        Card[i]='Thank you, '+Arr[i]+', for the wonderful '+event+' gift!';
+       
+    }
+       
+       return Card;
+}
+ 
+
+writeCards(['Ali', 'Mohammed', 'Amro'],'birthday');
+ console.log(Card);
+function countdown(Num){
+    let i=Num;
+    while(i>=0){
+        console.log(i);
+        i--;
+    }
+}
+countdown(10);
